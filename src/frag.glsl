9383035111c03270f123a588e818d4a6aa065382
OsakiTsukiko <osakitsukiko.mail@gmail.com>
2025-06-17 02:38:31 +0300

    shaders or sum

--- src/frag.glsl
+++ src/frag.glsl
@@ -3,7 +3,10 @@ out vec4 FragColor;
 
 in vec3 vColor;
 
+uniform vec3 testColor;
+
 void main()
 {
+    FragColor = vec4(testColor, 1.0f);
     FragColor = vec4(vColor, 1.0f);
 }
--- src/main.zig
+++ src/main.zig
@@ -6,10 +6,10 @@ const vertex_shader_source = @embedFile("./vert.glsl");
 const fragment_shader_source = @embedFile("./frag.glsl");
 
 const vertices = &[_]f32{
-    0.5, 0.5, 0.0, // top right
-    0.5, -0.5, 0.0, // bottom right
-    -0.5, -0.5, 0.0, // bottom left
-    -0.5, 0.5, 0.0, // top left
+    0.5, 0.5, 0.0, 1.0, 0.0, 0.0, // top right
+    0.5, -0.5, 0.0, 0.0, 1.0, 0.0, // bottom right
+    -0.5, -0.5, 0.0, 0.0, 0.0, 1.0, // bottom left
+    -0.5, 0.5, 0.0, 1.0, 0.0, 1.0, // top left
 };
 
 const indices = &[_]c_uint{
@@ -86,12 +86,18 @@ pub fn main() !void {
     gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, ebo);
     gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, indices.len * @bitSizeOf(c_uint), indices, gl.STATIC_DRAW);
 
-    gl.vertexAttribPointer(0, 3, gl.FLOAT, gl.FALSE, 3 * @sizeOf(f32), null);
+    // position attribute
+    gl.vertexAttribPointer(0, 3, gl.FLOAT, gl.FALSE, 6 * @sizeOf(f32), null);
     gl.enableVertexAttribArray(0);
+    // color attribute
+    gl.vertexAttribPointer(1, 3, gl.FLOAT, gl.FALSE, 6 * @sizeOf(f32), @ptrFromInt(3 * @sizeOf(f32)));
+    gl.enableVertexAttribArray(1);
 
     gl.bindVertexArray(0);
     gl.bindBuffer(gl.ARRAY_BUFFER, 0);
     gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, 0);
+
+    const colorUniformLocation = gl.getUniformLocation(shader_program, "testColor");
 
     while (!win.shouldClose()) {
         processInput(win);
@@ -99,13 +105,18 @@ pub fn main() !void {
         gl.clearColor(0.2, 0.3, 0.3, 1.0);
         gl.clear(gl.COLOR_BUFFER_BIT);
 
-        gl.polygonMode(gl.FRONT_AND_BACK, gl.LINE);
+        // gl.polygonMode(gl.FRONT_AND_BACK, gl.LINE);
+
+        const time_val = glfw.getTime();
+        const green_val = std.math.sin(time_val) / 2.0 + 0.5;
         gl.useProgram(shader_program);
+        gl.uniform3f(colorUniformLocation, 0.0, @floatCast(green_val), 0.0);
         gl.bindVertexArray(vao);
         // gl.drawArrays(gl.TRIANGLES, 0, 3);
         gl.drawElements(gl.TRIANGLES, 6, gl.UNSIGNED_INT, null);
         gl.bindVertexArray(0);
-        gl.polygonMode(gl.FRONT_AND_BACK, gl.FILL);
+
+        // gl.polygonMode(gl.FRONT_AND_BACK, gl.FILL);
 
         glfw.pollEvents();
         win.swapBuffers();
